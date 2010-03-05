14288fee21b6d3d9ddac87a0fbfd1322b4a6c9e9
Sam Lown <sam@sams-imac-desktop.(none)>
2010-03-05 01:45:25 +0000

    Improving general testing

--- spec/will_paginate_couchrest_spec.rb
+++ spec/will_paginate_couchrest_spec.rb
@@ -26,6 +26,22 @@ describe CouchRest::Mixins::WillPaginate do
     # @some_doc.stub(:id).and_return(123)
   end
 
+  it "should accept request when no results" do
+    docs = SomeDoc.paginate_by_name(:per_page => 5)
+    docs.total_entries.should eql(0)    
+  end
+
+  it "should accept request without page" do
+    docs = nil
+    lambda { docs = SomeDoc.paginate_by_name(:per_page => 5) }.should_not raise_error
+    docs.current_page.should eql(1)
+  end
+
+  it "should throw an exception when missing per_page parameter" do
+    lambda { SomeDoc.paginate_by_name() }.should raise_error
+  end
+
+
   describe "performing pagination with lots of documents" do
 
     before(:each) do
@@ -38,7 +54,7 @@ describe CouchRest::Mixins::WillPaginate do
 
     it "should produce a will paginate collection" do
       docs = SomeDoc.paginate_by_name( :page => 1, :per_page => 5 )
-      docs.class.should eql(::WillPaginate::Collection)
+      docs.should be_a_kind_of(::WillPaginate::Collection)
       docs.total_pages.should eql(4)
       docs.first.name.should eql('document 00')
       docs.length.should eql(5)
@@ -59,9 +75,6 @@ describe CouchRest::Mixins::WillPaginate do
       docs.total_entries.should eql(20)
       docs.length.should eql(5)
     end
-
-
-  
 
   end
 end
